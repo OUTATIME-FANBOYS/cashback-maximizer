0cb0b02496ff7786a341633402cb46228c5871a1
Tanmay Dureja <tanmaydureja@gmail.com>
2026-06-09 13:11:18 -0700

    Fix export rules

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.cashbackmaximizer',
   appName: 'CardSense',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'https://cashback-maximizer-flax.vercel.app',
+    cleartext: false,
+  },
 };
 
 export default config;
